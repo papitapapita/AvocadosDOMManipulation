1a445837f399e23634a8ab0b244affc9505b83d8
Jonathan <many.d.r.o.2001@hotmail.com>
2023-04-19 11:51:04 -0500

    Stylized and API results added to a container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const baseUrl = 'https://platzi-avo.vercel.app'
+const url = `${baseUrl}/api/avo`;
+const appNode = document.querySelector('#app');
 
-console.log('Happy hacking :)')
+async function fetchData(apiUrl){
+    const response = await window.fetch(apiUrl);
+    const data = await response.json();
+    //const listItems = [];
+    const fragment = new DocumentFragment();
+
+    data.data.forEach(element => {
+        const img = document.createElement('img');
+        img.src = `${baseUrl}${element.image}`;
+
+        const title = document.createElement('h2');
+        title.textContent = element.name;
+
+        const price = document.createElement('div');
+        price.textContent = element.price;
+
+        const container = document.createElement('div');
+        container.append(img, title, price);
+
+        fragment.append(container);
+    });
+
+    appNode.append(fragment);
+}
+
+fetchData(url);
